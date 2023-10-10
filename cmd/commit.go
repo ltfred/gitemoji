ab4806877609974500195bd3c98379b57b80340d
ltfred <ltfred@163.com>
2023-10-10 10:25:55 +0800

    ✨Add commit cmd

--- cmd/commit.go
+++ cmd/commit.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/manifoldco/promptui"
 
 	"github.com/spf13/cobra"
 )
@@ -10,11 +18,59 @@ import (
 var commitCmd = &cobra.Command{
 	Use:   "commit",
 	Short: "Interactively commit using the prompts.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("commit called")
-	},
+	Run:   commit,
 }
 
 func init() {
 	rootCmd.AddCommand(commitCmd)
+}
+
+func commit(cmd *cobra.Command, args []string) {
+	var data Data
+	if err := json.Unmarshal(emojis, &data); err != nil {
+		Error(cmd, args, err)
+	}
+
+	idx, err := fuzzyfinder.FindMulti(
+		data.GitEmojis,
+		func(i int) string {
+			return strings.Join([]string{data.GitEmojis[i].Emoji, data.GitEmojis[i].Description},
+				" - ")
+		})
+	if err != nil {
+		Error(cmd, args, err)
+	}
+
+	moji := data.GitEmojis[idx[0]]
+	blue := color.New(color.FgBlue).SprintFunc()
+	fmt.Println(fmt.Sprintf("Selected： %v - %v", moji.Emoji, blue(moji.Description)))
+
+	prompt := promptui.Prompt{
+		Label: "Enter the commit title",
+		Validate: func(s string) error {
+			if s == "" {
+				return errors.New("invalid commit title")
+			}
+			return nil
+		},
+	}
+
+	title, err := prompt.Run()
+	if err != nil {
+		Error(cmd, args, err)
+	}
+
+	prompt = promptui.Prompt{
+		Label: "Enter the commit message",
+	}
+
+	msg, err := prompt.Run()
+	if err != nil {
+		Error(cmd, args, err)
+	}
+
+	commandArgs := []string{"commit", "-a", "-m", moji.Emoji + title, "-m", msg}
+	if err = exec.Command("git", commandArgs...).Run(); err != nil {
+		Error(cmd, args, err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ltfred/gitemoji
 go 1.21.0
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.5.3 // indirect
@@ -10,6 +11,7 @@ require (
 	github.com/ktr0731/go-ansisgr v0.1.0 // indirect
 	github.com/ktr0731/go-fuzzyfinder v0.7.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
